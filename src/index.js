0a19f2180536943eaedd66ad55abe36b008b2305
Anya Bu <annabukhtiiarova@gmail.com>
2022-04-03 16:36:20 +0300

    webpack plus styles setted

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.scss';
+
+async function check() {
+  await fetch ('https://google.com');
+}
+
+check().then(()=> {
+  console.log('success');
+}).catch(()=> {
+  console.log('error');
+})
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const {resolve, join} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: resolve(__dirname, 'build'),
+      filename: '[name]-[fullhash].js',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ["@babel/plugin-transform-runtime"]
+            }
+          }
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html"
+      }),
+      new MiniCssExtractPlugin ( {
+        filename: '[name]-[fullhash].css'
+      })
+    ],
+    devServer: {
+      static: {
+        directory: join(__dirname, 'src')
+    },
+      port: 8080,
+  },
+  devtool: 'inline-source-map',
+}
